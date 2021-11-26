fd3a9995c5f82d78119bc48f54772f8a71db03d5
EMMANUEL SAENZ RANGEL <emmanuel.rangel@telcel.com>
2021-11-26 12:33:06 -0600

    Cambios actualizados al 26/11/2021

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = (props) => {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <title>Portafolio de proyectos</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Concert+One&family=Fredoka+One&family=Itim&family=Patrick+Hand&family=Roboto:wght@300;400;500;700&display=swap"
           rel="stylesheet"
